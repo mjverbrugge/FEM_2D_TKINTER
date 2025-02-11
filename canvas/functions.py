ff03a2d9feb0069e9905c8a6989c8af277b5aab8
mjverbrugge <marnix.verbrugge@hotmail.nl>
2025-02-11 09:55:19 +0100

    UpdateZoomToMousePosition

--- canvas/functions.py
+++ canvas/functions.py
@@ -1,8 +1,12 @@
+# General imports
+import copy
+
+# Custom imports
+import canvas.data as canvasData
+
 ###
 # Draw
 ###
-
-import canvas.data as canvasData
 
 def draw(myCanvas):
     myCanvas.delete("all")
@@ -30,10 +34,7 @@ def draw(myCanvas):
 # Mouse data
 ###
 
-
 # PAN #
-
-import canvas.data as canvasData
 
 def getCoordinates(event):
     w = event.widget
@@ -69,12 +70,26 @@ def drag(event):
 def zoom(event):
     # Update scale based on zoom input
 
+    vp = canvasData.viewpoint
+    # Reconstruct mouse position
+    x, y = getCoordinates(event)
+    # Save initial scale factor
+    scaleInitial = copy.deepcopy(canvasData.scale)
+
+
     # Zoom-in
     if event.delta>0 : canvasData.scale += canvasData.scrollSensitivity
     # Zoom-out
     elif canvasData.scale == 0.0001: return
     elif canvasData.scale-canvasData.scrollSensitivity <= 0: canvasData.scale = 0.0001
     else: canvasData.scale -= canvasData.scrollSensitivity
+
+    # Scale to mouse
+    sR = canvasData.scale/scaleInitial # Scale ratio
+
+    #canvasData.viewpoint[0] = vp[0]-xDif
+    canvasData.viewpoint[0] = vp[0]*sR + (sR-1)*x
+    canvasData.viewpoint[1] = vp[1]*sR + (sR-1)*y
 
     # Redraw canvas
     w = event.widget
